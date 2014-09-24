d188972e7ffff31a17a55254dce968181551bed7
Randy Patterson Jr. <rpattjr@gmail.com>
2014-09-24 13:56:08 -0400

    Add initial package config files.

---
+++ exercism-io-cli/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'exercism-io-cli'
+$url = 'https://github.com/exercism/cli/releases/download/v1.7.2/exercism-windows-32bit.tgz'
+$url64 = 'https://github.com/exercism/cli/releases/download/v1.7.2/exercism-windows-64bit.tgz'
+
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
+
+try {
+  $binaryTar = $installDir + "\" + $packageName + "Install"
+  Get-ChocolateyUnzip "$binaryTar"  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
