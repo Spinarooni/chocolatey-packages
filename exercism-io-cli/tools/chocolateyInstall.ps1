e1b646488d911065034fdfdd7d9aacb3f02c457a
Randy Patterson Jr. <rpattjr@gmail.com>
2014-09-27 16:23:55 -0400

    Fix install process due to change in binary packaging.

--- exercism-io-cli/tools/chocolateyInstall.ps1
+++ exercism-io-cli/tools/chocolateyInstall.ps1
@@ -1,14 +1,6 @@
 ﻿$packageName = 'exercism-io-cli'
-$url = 'https://github.com/exercism/cli/releases/download/v1.7.3/exercism-windows-32bit.tgz'
-$url64 = 'https://github.com/exercism/cli/releases/download/v1.7.3/exercism-windows-64bit.tgz'
+$url = 'https://github.com/exercism/cli/releases/download/v1.7.3/exercism-windows-32bit.zip'
+$url64 = 'https://github.com/exercism/cli/releases/download/v1.7.3/exercism-windows-64bit.zip'
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
-
-try {
-  $binaryTar = $installDir + "\" + $packageName + "Install"
-  Get-ChocolateyUnzip "$binaryTar"  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-} catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
-}
