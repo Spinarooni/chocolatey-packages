53ec29b51333b0af519600d3618b421f537a96de
Randy Patterson Jr. <rpattjr@gmail.com>
2014-09-28 08:10:49 -0400

    Update exercism CLI package to 1.7.4.

--- exercism-io-cli/tools/chocolateyInstall.ps1
+++ exercism-io-cli/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'exercism-io-cli'
-$url = 'https://github.com/exercism/cli/releases/download/v1.7.3/exercism-windows-32bit.zip'
-$url64 = 'https://github.com/exercism/cli/releases/download/v1.7.3/exercism-windows-64bit.zip'
+$url = 'https://github.com/exercism/cli/releases/download/v1.7.4/exercism-windows-32bit.zip'
+$url64 = 'https://github.com/exercism/cli/releases/download/v1.7.4/exercism-windows-64bit.zip'
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
