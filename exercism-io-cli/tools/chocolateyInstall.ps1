3a705880cdacadfd9c05eda80671c16302648a62
Randy Patterson Jr <rpattjr@gmail.com>
2015-12-28 08:23:33 -0500

    Update exercism package to 2.2.3. Add more metadata for the package.

--- exercism-io-cli/tools/chocolateyInstall.ps1
+++ exercism-io-cli/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'exercism-io-cli'
-$url = 'https://github.com/exercism/cli/releases/download/v2.2.1/exercism-windows-32bit.zip'
-$url64 = 'https://github.com/exercism/cli/releases/download/v2.2.1/exercism-windows-64bit.zip'
+$url = 'https://github.com/exercism/cli/releases/download/v2.2.3/exercism-windows-32bit.zip'
+$url64 = 'https://github.com/exercism/cli/releases/download/v2.2.3/exercism-windows-64bit.zip'
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
