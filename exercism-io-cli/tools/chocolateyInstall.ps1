67ab3ed1fa493424a936e1ce5f619e9b0b62d44a
Randy Patterson Jr. <rpattjr@gmail.com>
2024-10-01 16:27:25 -0400

    Update the exercism cli pakcage to 3.5.0.

--- exercism-io-cli/tools/chocolateyInstall.ps1
+++ exercism-io-cli/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'exercism-io-cli'
-$url = 'https://github.com/exercism/cli/releases/download/v3.4.0/exercism-3.4.0-windows-32bit.zip'
-$url64 = 'https://github.com/exercism/cli/releases/download/v3.4.0/exercism-3.4.0-windows-64bit.zip'
-$checksum = 'F8E8A0FD8F3FDDEFE253E934D6281A3431B137DBB3D848F850EC2D62DEEA0778'
-$checksum64 = '9EF4DFC6F17034DBA48AE28FE46FA139EFE8887B02F6A845B38EF181CC98DAF5'
+$url = 'https://github.com/exercism/cli/releases/download/v3.5.0/exercism-3.5.0-windows-32bit.zip'
+$url64 = 'https://github.com/exercism/cli/releases/download/v3.5.0/exercism-3.5.0-windows-64bit.zip'
+$checksum = 'F0767C2E84AB492EA433D706D033669F217130EAE484C0B5A3BB8CEFCBE9C3BA'
+$checksum64 = 'B01600BA4C4E5A830B0CD62AEEF9CD4385FD8EE9C948AEF1C5E246D7F4E3C145'
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
